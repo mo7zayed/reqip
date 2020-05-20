ae375eb9322b4a2a38db7e6bd73452e56049cccd
mo7zayed <mohamedzayed709@yahoo.com>
2020-05-20 10:42:30 +0200

    Added go.mod & enhance GetClientIP() code

--- reqip.go
+++ reqip.go
@@ -53,55 +53,30 @@ func GetClientIP(r *http.Request) string {
 	headers := r.Header
 
 	if len(headers) > 0 {
-		// Standard headers used by Amazon EC2, Heroku, and others.
-		if ip := r.Header.Get("x-client-ip"); isIP(ip) {
-			return ip
+		checklist := []string{
+			"x-client-ip", // Standard headers used by Amazon EC2, Heroku, and others.
+			"x-forwarded-for", // Load-balancers (AWS ELB) or proxies.
+			"cf-connecting-ip", // @see https://support.cloudflare.com/hc/en-us/articles/200170986-How-does-Cloudflare-handle-HTTP-Request-headers-
+			"fastly-client-ip", // Fastly and Firebase hosting header (When forwared to cloud function)
+			"true-client-ip", // Akamai and Cloudflare: True-Client-IP.
+			"x-real-ip", // Default nginx proxy/fcgi; alternative to x-forwarded-for, used by some proxies.
+			"x-cluster-client-ip", // (Rackspace LB and Riverbed's Stingray) http://www.rackspace.com/knowledge_center/article/controlling-access-to-linux-cloud-sites-based-on-the-client-ip-address
+			"x-forwarded",
+			"forwarded-for",
+			"forwarded",
 		}
 
-		// Load-balancers (AWS ELB) or proxies.
-		if ip := getClientIPFromXForwardedFor(r.Header.Get("x-forwarded-for")); isIP(ip) {
-			return ip
-		}
+		for _, h := range checklist {
+			if h == "x-forwarded-for" {
+				if ip := getClientIPFromXForwardedFor(r.Header.Get(h)); isIP(ip) {
+					return ip
+				}
+				continue
+			}
 
-		// Cloudflare.
-		// @see https://support.cloudflare.com/hc/en-us/articles/200170986-How-does-Cloudflare-handle-HTTP-Request-headers-
-		// CF-Connecting-IP - applied to every request to the origin.
-		if ip := r.Header.Get("cf-connecting-ip"); isIP(ip) {
-			return ip
-		}
-
-		// Fastly and Firebase hosting header (When forwared to cloud function)
-		if ip := r.Header.Get("fastly-client-ip"); isIP(ip) {
-			return ip
-		}
-
-		// Akamai and Cloudflare: True-Client-IP.
-		if ip := r.Header.Get("true-client-ip"); isIP(ip) {
-			return ip
-		}
-
-		// Default nginx proxy/fcgi; alternative to x-forwarded-for, used by some proxies.
-		if ip := r.Header.Get("x-real-ip"); isIP(ip) {
-			return ip
-		}
-
-		// (Rackspace LB and Riverbed's Stingray)
-		// http://www.rackspace.com/knowledge_center/article/controlling-access-to-linux-cloud-sites-based-on-the-client-ip-address
-		// https://splash.riverbed.com/docs/DOC-1926
-		if ip := r.Header.Get("x-cluster-client-ip"); isIP(ip) {
-			return ip
-		}
-
-		if ip := r.Header.Get("x-forwarded"); isIP(ip) {
-			return ip
-		}
-
-		if ip := r.Header.Get("forwarded-for"); isIP(ip) {
-			return ip
-		}
-
-		if ip := r.Header.Get("forwarded"); isIP(ip) {
-			return ip
+			if ip := r.Header.Get(h); isIP(ip) {
+				return ip
+			}
 		}
 	}
 
